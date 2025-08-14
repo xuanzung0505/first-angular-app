16e5971031200d3971e9601a1301898f27016328
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2025-08-14 14:55:50 +0700

    Implement housing location feature with dynamic listing and styling updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {
+  HousingLocationInfo,
+  housingLocationList,
+} from '../models/housinglocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocations: HousingLocationInfo[] = [...housingLocationList];
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocationInfo } from '../models/housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   templateUrl: './housing-location.component.html',
-  styleUrls: ['./housing-location.component.scss']
+  styleUrls: ['./housing-location.component.scss'],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocationInfo;
 }
