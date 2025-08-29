c529a9af0286f6a1f745a4883a116ba415d78898
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2025-08-29 14:23:51 +0700

    Add Vietnamese translation support and update app component for i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import translationsVi from '../assets/i18n/vi.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'first-app';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'vi']);
+    this.translate.setTranslation('vi', translationsVi);
+    this.translate.setDefaultLang('en');
+    this.translate.use('vi');
+  }
 }
